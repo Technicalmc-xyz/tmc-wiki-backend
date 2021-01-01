4c97a4f87c773dba94a98b02bbf18806b13c3e13
Fx Morin <fx.e.morin@gmail.com>
2020-12-31 20:01:17 -0500

    Added Ranks

--- users.js
+++ users.js
@@ -2,11 +2,12 @@
 const usersById = new Map();
 
 class User {
-    constructor(discordId, discordName, discordDiscriminator, discordAvatar) {
+    constructor(discordId, discordName, discordDiscriminator, discordAvatar, rank) {
         this.discordId = discordId;
         this.discordName = discordName;
         this.discordDiscriminator = discordDiscriminator;
         this.discordAvatar = discordAvatar;
+		this.rank = rank;
         usersById.set(discordId, this);
     }
 
