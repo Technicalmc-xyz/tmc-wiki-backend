75ac2791753fb1c1d2f1e4d612f266641b7e5bb0
Jack <jackbaude@gmail.com>
2021-01-26 13:22:44 -0600

    Refactors for clarity

--- index.ts
+++ index.ts
@@ -1,15 +1,16 @@
 // Setting up the authentication library
 const sqlite3 = require('sqlite3');
-const db = new sqlite3.Database('Authentication.db', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (err) => {
+
+const userdatabase = new sqlite3.Database('Authentication.db', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (err) => {
     if (err) {
         console.error(err.message);
     }
-    console.log(chalk.greenBright('Connected to the authentication database.'));
+    console.log(magenta('Connected to the authentication database.'));
 });
 
 // Creating the table for the first time, also shows the table structure. Lots of info in case you want to add stuff in the future (forward thinking)
-db.serialize(() => {
-    db.run('CREATE TABLE IF NOT EXISTS "accounts" ("id" VARCHAR(20) NOT NULL UNIQUE, "username" VARCHAR(50) NOT NULL, "discriminator" SMALLINT NOT NULL, "avatar" VARCHAR NOT NULL,"mcusername" VARCHAR(16), "links" TEXT, "rank" TINYINT DEFAULT `guest`, "added" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY("id"));');
+userdatabase.serialize(() => {
+    userdatabase.run('CREATE TABLE IF NOT EXISTS "accounts" ("id" VARCHAR(20) NOT NULL UNIQUE, "username" VARCHAR(50) NOT NULL, "discriminator" SMALLINT NOT NULL, "avatar" VARCHAR NOT NULL,"mcusername" VARCHAR(16), "links" TEXT, "rank" TINYINT DEFAULT `guest`, "added" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY("id"));');
 });
 
 const {randomBytes} = require('crypto');
@@ -21,11 +22,11 @@ const passport = require('passport');
 const querystring = require('querystring');
 const DiscordStrategy = require('passport-discord').Strategy;
 const session = require('express-session');
-const chalk = require('chalk');
+const {greenBright, magenta, red} = require('chalk')
 const xss = require('xss-clean');
 const fileUpload = require('express-fileupload');
 
-const posts = require('./src/posts');
+const articles = require('./src/artciles');
 const archive = require('./src/archive');
 const utils = require('./utils/utils');
 
@@ -35,7 +36,7 @@ const rankList= ['banned', 'guest', 'trusted', 'editor', 'dev', 'mod']; // 0=ban
 let production: boolean = false;
 let development: boolean = false;
 if (process.argv.length === 2) {
-    console.error('Expected at least one argument!');
+    console.error(red('Expected at least one argument!'));
     process.exit(1);
 }
 // Gets the third argument and makes either production or development true
@@ -102,59 +103,61 @@ const handleCreatePost = async (req, res) => {
     const title = utils.cast('string', reqBody.title);
     const tags = utils.cast('string', reqBody.tags);
     const description = utils.cast('string', reqBody.description);
-    const metadata = await posts.createPost(req.user, title, description, tags, body);
+    const metadata = await articles.createPost(req.user, title, description, tags, body);
     console.log(`Created post #${metadata.id}`);
     res.send('OK');
 };
 
 
-const getPost = async (req, res) => {
-    const postId = utils.cast('number', +utils.cast('object', req.query).id);
-    if (!posts.postExists(postId)) {
-        res.status(404).send(`No such post ID ${postId}`);
-        return;
-    }
-    const networkPost = await posts.getNetworkPostObject(postId);
-    // See what post number is being requested
-    // Console.log('Post ID Number: ' + postId);
-    res.send(networkPost);
-};
+
 
 
 const editPost = async (req, res) => {
     const postId = utils.cast('number', +utils.cast('object', req.query).id);
-    if (!posts.postExists(postId)) {
+    if (!articles.postExists(postId)) {
         res.status(404).send(`No such post ID ${postId}`);
         return;
     }
     const reqBody = utils.cast('object', req.body);
-    const metadata = posts.getMetadata(postId);
+    const metadata = articles.getMetadata(postId);
     if (metadata.edit_count !== utils.cast('number', req.body.lastEditCount)) {
         res.send('OUTDATED');
         return;
     }
     metadata.edit_count++;
 
-    await posts.setPostBody(postId, utils.cast('string', reqBody.body));
+    await articles.setPostBody(postId, utils.cast('string', reqBody.body));
     const message = utils.cast('string', reqBody.message);
     metadata.title = utils.cast('string', reqBody.title);
     metadata.tags = utils.cast('string', reqBody.tags);
     metadata.description = utils.cast('string', reqBody.description);
     metadata.last_edited = new Date().toDateString();
-    await posts.saveMetadata();
-    await posts.commit(postId, message, req.user);
+    await articles.saveMetadata();
+    await articles.commit(postId, message, req.user);
     console.log(`Edited post #${postId}`)
     res.send('OK');
 };
 
-const getPosts = async (req, res) => {
+const getArticle = async (req, res) => {
+    const postId = utils.cast('number', +utils.cast('object', req.query).id);
+    if (!articles.postExists(postId)) {
+        res.status(404).send(`No such post ID ${postId}`);
+        return;
+    }
+    const networkPost = await articles.getNetworkPostObject(postId);
+    // See what post number is being requested
+    // Console.log('Post ID Number: ' + postId);
+    res.send(networkPost);
+};
+
+const getArticles = async (req, res) => {
     const query = utils.cast('object', req.query);
     let n = utils.cast('number', +query.n) || 5;
     if (n < 0) n = 5;
     let start = utils.cast('number', +query.start) || 0;
     if (start < 0) start = 0;
     const q = utils.cast('string', query.q);
-    let myPosts = posts.getAllMetadata();
+    let myPosts = articles.getAllMetadata();
 
     if (q) {
         // TODO: sorting
@@ -172,7 +175,6 @@ const getPosts = async (req, res) => {
     myPosts = myPosts.slice(length - start - n, length - start);
     res.send(myPosts);
 };
-
 
 const getUserInfo = (req, res) => {
     let userInfo = {
@@ -206,9 +208,9 @@ interface LoginDatabaseParams {
     avatar: string;
 }
 const loginDatabase = ({id, username, discriminator, avatar}: LoginDatabaseParams) => {
-    db.get('SELECT id FROM accounts WHERE id = ?;', [id], (err, row) => {
+    userdatabase.get('SELECT id FROM accounts WHERE id = ?;', [id], (err, row) => {
         if (row == undefined) { // If user does not exist
-            db.run('insert into accounts(id,username,discriminator,avatar) values (?,?,?,?)', [id, username, discriminator, avatar], (err) => {
+            userdatabase.run('insert into accounts(id,username,discriminator,avatar) values (?,?,?,?)', [id, username, discriminator, avatar], (err) => {
                 if (err) {
                     console.error(err.message);
                 }
@@ -216,7 +218,7 @@ const loginDatabase = ({id, username, discriminator, avatar}: LoginDatabaseParam
                 // Rank would be "guest" so do nothing
             });
         } else { // If user exists, overwrite the database with the their information, in case they updated their profile
-            db.run('UPDATE accounts set username = ?, discriminator =?, avatar = ? WHERE id = ?', [username, discriminator, avatar, id], (err) => {
+            userdatabase.run('UPDATE accounts set username = ?, discriminator =?, avatar = ? WHERE id = ?', [username, discriminator, avatar, id], (err) => {
                 if (err) {
                     console.error(err.message);
                 }
@@ -256,11 +258,11 @@ const modifyPermissions = (req, res) => {
         return res.status(403).send('DISCORD ID IS TOO LARGE');
     }
     else {
-        db.run('UPDATE accounts set rank = ? WHERE id = ?', [rank, id], (err) => {
+        userdatabase.run('UPDATE accounts set rank = ? WHERE id = ?', [rank, id], (err) => {
             if (err) {
                 return res.status(403).send(err);
             }
-            console.log((chalk.green('Changed Rank for id: ' + id + ' to ' + rank)));
+            console.log(greenBright('Changed Rank for id: ' + id + ' to ' + rank));
             return res.status(200).send('Changed Rank for id: ' + id + ' to ' + rank);
         });
     }
@@ -274,7 +276,7 @@ interface User {
 }
 // Will return all information on a single user in the database.
 const getUser = ({id, callback}: User) => {
-    db.get("SELECT * FROM accounts WHERE id = ?", [id], (err, row) => {
+    userdatabase.get("SELECT * FROM accounts WHERE id = ?", [id], (err, row) => {
         if (row == undefined) { // No users exist
             return callback(null);
         }  // If user exists, just assign
@@ -288,7 +290,7 @@ const getUser = ({id, callback}: User) => {
 // If no rank is specified it will return a list of all users. Otherwise it will return a list of all users with that rank
 // Returns a JSON list of users, each user has a DiscordId, Username, and Rank. For more info about a user, do getUser(discordId)
 const getUsers = (req, res) =>
-    db.all('SELECT id,username,rank FROM accounts ORDER BY rank,id', [], (err, rows) => {
+    userdatabase.all('SELECT id,username,rank FROM accounts ORDER BY rank,id', [], (err, rows) => {
         if (rows == undefined) { // No users exist, excuse me... WHAT?
             res.send('No users in database with that rank?');
         } else { // If user exists, just assign
@@ -297,20 +299,20 @@ const getUsers = (req, res) =>
     });
 
 const userExists = async (id: string): Promise<boolean> =>
-    await db.get('SELECT * FROM accounts WHERE id = ?', [id], (err, row) => {
+    await userdatabase.get('SELECT * FROM accounts WHERE id = ?', [id], (err, row) => {
         return row !== undefined;
     });
 
 
 const urlPrefix = production ? '/' : '/api/';
 
-app.get(urlPrefix + '__getpost__', getPost);
+app.get(urlPrefix + '__getpost__', getArticle);
 
 app.post(urlPrefix + '__newpost__', requirePermission('guest'), handleCreatePost);
 
 app.post(urlPrefix + '__editpost__', requirePermission('guest'), editPost);
 
-app.get(urlPrefix + '__listposts__', getPosts);
+app.get(urlPrefix + '__listposts__', getArticles);
 
 app.get(urlPrefix + 'archive/:fileName', archive.download);
 app.get(urlPrefix + 'archive', archive.index);
